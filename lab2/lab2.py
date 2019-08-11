2d35523e8651c4a356867a61c537d4a38543fc7f
Larry8799 <larry.cs06@nctu.edu.tw>
2019-08-11 21:44:50 +0800

    add save func

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -19,18 +19,20 @@ if __name__ == "__main__":
         "relu" : EEGNet.Net(nn.ReLU).to(device),
         "leaky_relu" : EEGNet.Net(nn.LeakyReLU).to(device),
     }
-    Accs = run.runModels(models, epoch_size=300, batch_size=64, learning_rate=1e-2, show=True)
+    Accs = run.runModels(models, epoch_size=150, batch_size=64, learning_rate=1e-2, show=True)
     showstuff.showAccuracy("EEGNet", **Accs)
     AccRes.add("EEGNet", Accs, show=True)
-    
+    torch.save(models, 'EEG.pkl');
+
     print('Training & Testing DeepConvNet')
     models = {
         "elu" : DeepConvNet.Net(nn.ELU).to(device),
         "relu" : DeepConvNet.Net(nn.ReLU).to(device),
         "leaky_relu" : DeepConvNet.Net(nn.LeakyReLU).to(device),
     }
-    Accs = run.runModels(models, epoch_size=300, batch_size=64, learning_rate=1e-3, show=True)
+    Accs = run.runModels(models, epoch_size=150, batch_size=64, learning_rate=1e-3, show=True)
     showstuff.showAccuracy("DeepConvNet", **Accs)
     AccRes.add("DeepConvNet", Accs, show=True)
+    torch.save(models, 'DeepConv.pkl');
     
     AccRes.show()
